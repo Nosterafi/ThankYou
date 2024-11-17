c1beb868c4d038a3ec92cb8391a7fea19281ec68
Adel <adelfat71@mail.ru>
2024-11-17 15:45:13 +0400

    Add view models

---
+++ OnlineTipsService/ViewModels/PaymentGateway.cs
@@ -0,0 +1,37 @@
+﻿using Npgsql.PostgresTypes;
+using OnlineTipsService.Domain;
+using OnlineTipsService.App;
+using System;
+
+namespace ThankYou.ViewModels
+{
+    public class PaymentGateway
+    {
+        private readonly Tip tip;
+        private bool IsAvailable { get; set; }
+
+        public PaymentGateway(Tip tip)
+        {
+            this.tip = tip;
+        }
+
+        
+
+        public string DoOperation()
+        {
+            var res = "Exception";
+            //Имитация работы порта
+
+            if (IsAvailable)
+            {
+                using (PostgresContext db = new PostgresContext())
+                {
+                    db.Tips.Add(tip);
+                    res = "Операция прошла успешно! Спасибо за отправленные чаевые";
+                }
+            }
+
+            return res;
+        }
+    }
+}
