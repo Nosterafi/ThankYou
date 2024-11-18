c108c6f71c436da6ce025455e747a61ba05f05b6
Nosterafi <nosterafi56@gmail.com>
2024-11-18 19:53:49 +0400

    Реализовал заполнение страницы оплаты

--- ThankYou/ThankYou/Controllers/MainController.cs
+++ ThankYou/ThankYou/Controllers/MainController.cs
@@ -3,16 +3,15 @@ using System.Diagnostics;
 using ThankYou.Models;
 using ThankYou.DB.Context;
 using ThankYou.DB.Domain;
+using ThankYou.ViewModels;
 
 namespace ThankYou.Controllers
 {
     public class MainController : Controller
     {
+        private static readonly PostgresContext _postgresContext = new();
+
         private readonly ILogger<MainController> _logger;
-
-        private readonly PostgresContext _postgresContext = new();
-
-        private Tip tip;
 
         public Employee Employee { get; set; }
 
@@ -28,26 +27,22 @@ namespace ThankYou.Controllers
             var employee = _postgresContext.Employees.Find(employeeId);
             employee.Merchant = _postgresContext.Merchants.Find(employee.MerchantId);
 
-            tip = new Tip();
+            var tip = new Tip();
             tip.Employee = employee;
-            tip.EmployeeId = employeeId;
 
-            return View("pay", tip);
+            return View("pay", new PayViewModel(tip));
         }
 
         [HttpPost]
-        public IActionResult SetSum(short sum) 
-        { 
-            tip.Sum = sum;
+        public IActionResult SendTip(PayViewModel viewModel) 
+        {
+            viewModel.Tip.Employee = employee;
+            
+            _postgresContext.Tips.Add(viewModel.Tip);
+            _postgresContext.SaveChanges();
 
-            return RedirectToAction("PayPage", tip.EmployeeId);
+            return RedirectToAction("MainPage");
         }
-
-        public IActionResult PayErrorPage() { return View("payError"); }
-
-        public IActionResult PayNotenoughPage() { return View("payNotenough"); }
-
-        public IActionResult PaySuccesPage() { return View("paySucces"); }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- ThankYou/ThankYou/obj/Debug/net8.0/ThankYou.AssemblyInfo.cs
+++ ThankYou/ThankYou/obj/Debug/net8.0/ThankYou.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThankYou")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bad270dd354179f285dcaabc5aed5479d73f859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44915861a63ec4a431e868a5933975ba495458c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThankYou")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThankYou")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
