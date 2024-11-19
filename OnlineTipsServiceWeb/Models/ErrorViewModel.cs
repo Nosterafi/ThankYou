ba8d8d48e2edb42a283ab0be41c80aed8606589e
Nosterafi <nosterafi56@gmail.com>
2024-11-19 18:20:27 +0400

    Добавил файл с базой данных

--- OnlineTipsServiceWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OnlineTipsServiceWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
